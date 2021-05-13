0c27c696e75ec8ee8aa99492e78b683be81e4a7a
Kogeki <sutter.trevor@gmail.com>
2021-05-13 15:39:13 -0400

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# Discord bot token
-TOKEN = "ODQyNDgxNTQyOTY1NDkzNzcw.YJ18FA.V_8eKf0iABafFRLIqMJ-3blxmDs"
-
-# Postgres DSN for the bot's database. It's needed for the bot to be able to store data in a SQL database. It looks like this: "postgres://[user]:[password]@[host]:[port]/[database name]" (Some options may not be needed)
-POSTGRES = "postgres://postgres:password@db/postgres"
